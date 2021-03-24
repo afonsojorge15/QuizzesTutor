480dcd4c88a6e02b44130239f5448150d946019b
TmsPaiva <tmspaiva@gmail.com>
2021-03-24 18:43:07 +0000

    Close #32

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/RemoveItemCombinationQuestion.groovy
@@ -0,0 +1,71 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.service
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Image
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.ItemCombinationQuestion
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Item
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Topic
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.TopicDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.questionsubmission.domain.QuestionSubmission
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.Quiz
+import pt.ulisboa.tecnico.socialsoftware.tutor.quiz.domain.QuizQuestion
+import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.User
+
+@DataJpaTest
+class RemoveItemCombinationQuestion extends SpockTest {
+    def question
+    def itemOK
+    def itemKO
+    def teacher
+
+    def setup() {
+        def image = new Image()
+        image.setUrl(IMAGE_1_URL)
+        image.setWidth(20)
+        imageRepository.save(image)
+
+        question = new Question()
+        question.setKey(1)
+        question.setTitle(QUESTION_1_TITLE)
+        question.setContent(QUESTION_1_CONTENT)
+        question.setStatus(Question.Status.AVAILABLE)
+        question.setNumberOfAnswers(2)
+        question.setNumberOfCorrect(1)
+        question.setCourse(externalCourse)
+        question.setImage(image)
+        def questionDetails = new ItemCombinationQuestion()
+        question.setQuestionDetails(questionDetails)
+        questionDetailsRepository.save(questionDetails)
+        itemRepository.save(question)
+
+
+
+        itemOK = new Item()
+        itemOK.setContent(ITEM_1_CONTENT)
+        itemOK.setGroup(true)
+        itemOK.setQuestionDetails(questionDetails)
+        itemRepository.save(itemOK)
+
+        itemKO = new Item()
+        itemKO.setContent(ITEM_2_CONTENT)
+        itemKO.setGroup(false)
+        itemKO.setQuestionDetails(questionDetails)
+        itemRepository.save(itemKO)
+    }
+
+    def "remove a question"() {
+        when:
+        questionService.removeQuestion(question.getId())
+
+        then: "the question is removeQuestion"
+        questionRepository.count() == 0L
+        imageRepository.count() == 0L
+        optionRepository.count() == 0L
+    }
