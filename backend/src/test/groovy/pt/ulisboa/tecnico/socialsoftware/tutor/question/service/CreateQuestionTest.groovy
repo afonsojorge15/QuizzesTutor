f023e718fa0d25b3cadb915ea198fa1772d11e4c
DuarteFaria95 <duarte.n.e.faria@gmail.com>
2021-03-25 17:18:34 +0000

    Close #33

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/CreateQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/CreateQuestionTest.groovy
@@ -396,6 +396,96 @@ class CreateQuestionTest extends SpockTest {
         exception.getErrorMessage() == ErrorMessage.AT_LEAST_THREE_SLOTS_NEEDED
     }
 
+    // tests for OpenAnserQuestion -----------------------------------------------------------------------------
+
+    def "create a open answer question with no image "() {
+        given: "a questionDto"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1)
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+        questionDto.setQuestionDetailsDto(new OpenAnswerQuestionDto())
+        and: 'an answer'
+        def  answer = new String(ANSWER_STRING_CONTENT)
+        questionDto.getQuestionDetailsDto().setAnwer(anwer)
+        when:
+        questionService.createQuestion(externalCourse.getId(), questionDto)
+
+        then: "the correct question is inside the repository"
+        questionRepository.count() == 1L
+
+    }
+
+    def "create a open answer question with no image and an invalid answer"() {
+        given: "a questionDto"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1)
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+        questionDto.setQuestionDetailsDto(new OpenAnswerQuestionDto())
+        and: 'an answer'
+        def  answer = ""
+        questionDto.getQuestionDetailsDto().setAnwer(anwer)
+        when:
+        questionService.createQuestion(externalCourse.getId(), questionDto)
+
+        then: "exception is thrown"
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == ErrorMessage.INVALID_ANSWER
+
+    }
+
+    def "create a open answer question with image "() {
+        given: "a questionDto"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1)
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+        questionDto.setQuestionDetailsDto(new OpenAnswerQuestionDto())
+        and: 'an image'
+        def image = new ImageDto()
+        image.setUrl(IMAGE_1_URL)
+        image.setWidth(20)
+        questionDto.setImage(image)
+        and: 'an answer'
+        def  answer = new String(ANSWER_STRING_CONTENT)
+        questionDto.getQuestionDetailsDto().setAnwer(anwer)
+        when:
+        questionService.createQuestion(externalCourse.getId(), questionDto)
+
+        then: "the correct question is inside the repository"
+        questionRepository.count() == 1L
+
+    }
+
+    def "create a open answer question with image and an invalid answer"() {
+        given: "a questionDto"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1)
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+        questionDto.setQuestionDetailsDto(new OpenAnswerQuestionDto())
+        and: 'an image'
+        def image = new ImageDto()
+        image.setUrl(IMAGE_1_URL)
+        image.setWidth(20)
+        questionDto.setImage(image)
+        and: 'an answer'
+        def  answer = ""
+        questionDto.getQuestionDetailsDto().setAnwer(anwer)
+        when:
+        questionService.createQuestion(externalCourse.getId(), questionDto)
+
+        then: "exception is thrown"
+        def exception = thrown(TutorException)
+        exception.getErrorMessage() == ErrorMessage.INVALID_ANSWER
+
+    }
+
 
     @Unroll
     def "fail to create any question for invalid/non-existent course (#nonExistentId)"(Integer nonExistentId) {
