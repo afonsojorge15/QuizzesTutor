a92d044f1dff42f6ae4603ff767d138db9adfe2c
Afonso-Jorge <afonso.matos.jorge@gmail.com>
2021-03-26 13:59:05 +0000

    Close #51

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/CreateItemCombinationQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/CreateItemCombinationQuestionTest.groovy
@@ -7,6 +7,7 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
 import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.CodeFillInQuestion
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.CodeOrderQuestion
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Item
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.ItemCombinationQuestion
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.*
@@ -26,19 +27,20 @@ class CreateItemCombinationQuestionTest extends SpockTest {
         questionDto.setQuestionDetailsDto(new ItemCombinationQuestionDto())
         and: 'two itemIDs'
         def itemDto1 = new ItemDto()
-        itemDto1.setContent(ITEM_1_CONTENT)
+        itemDto1.setContent(OPTION_1_CONTENT)
         itemDto1.setGroup(true)
         def itemDto2 = new ItemDto()
-        itemDto2.setContent(ITEM_2_CONTENT)
+        itemDto2.setContent(OPTION_2_CONTENT)
         itemDto2.setGroup(false)
         def items = new ArrayList<ItemDto>()
         items.add(itemDto1)
         items.add(itemDto2)
         def linkDto = new LinkDto()
-        linkDto.setLink(itemDto1.getId(), itemDto2.getId())
+        linkDto.setLink(itemDto1, itemDto2)
         def links = new ArrayList<LinkDto>()
         links.add(linkDto)
-        questionDto.getQuestionDetailsDto().setItems(items)
+        questionDto.getQuestionDetailsDto().setItemsL(items)
+        questionDto.getQuestionDetailsDto().setItemsR(items)
         questionDto.getQuestionDetailsDto().setLinks(links)
 
         when:
@@ -46,7 +48,8 @@ class CreateItemCombinationQuestionTest extends SpockTest {
 
         then: "the link exists between both items"
         def result = questionRepository.findAll().get(0)
-        result.getItems().size() == 2
+        result.getItemsL().size() == 1
+        result.getItemsR().size() == 1
         result.getLinks().size() == 1
         result.getLinks() == links
     }
@@ -70,7 +73,8 @@ class CreateItemCombinationQuestionTest extends SpockTest {
         items.add(itemDto1)
         items.add(itemDto2)
         def links = new ArrayList<LinkDto>()
-        questionDto.getQuestionDetailsDto().setItems(items)
+        questionDto.getQuestionDetailsDto().setItemsL(items)
+        questionDto.getQuestionDetailsDto().setItemsR(items)
         questionDto.getQuestionDetailsDto().setLinks(links)
 
         when:
@@ -79,7 +83,8 @@ class CreateItemCombinationQuestionTest extends SpockTest {
         then: "the link does not exist"
         def result = questionRepository.findAll().get(0)
         result.getLinks().size() == 0
-        result.getItems().size() == 2
+        result.getItemsL().size() == 1
+        result.getItemsR().size() == 1
     }
 
     def "create an item combo question with three and one item connecting to other 2 items"() {
@@ -106,12 +111,13 @@ class CreateItemCombinationQuestionTest extends SpockTest {
         items.add(itemDto3)
         def linkDto = new LinkDto()
         def linkDto2 = new LinkDto()
-        linkDto.setLink(itemDto1.getId(), itemDto2.getId())
-        linkDto2.setLink(itemDto1.getId(), itemDto3.getId())
+        linkDto.setLink(itemDto1, itemDto2)
+        linkDto2.setLink(itemDto1, itemDto3)
         def links = new ArrayList<LinkDto>()
         links.add(linkDto)
         links.add(linkDto2)
-        questionDto.getQuestionDetailsDto().setItems(items)
+        questionDto.getQuestionDetailsDto().setItemsL(items)
+        questionDto.getQuestionDetailsDto().setItemsR(items)
         questionDto.getQuestionDetailsDto().setLinks(links)
 
         when:
@@ -119,7 +125,8 @@ class CreateItemCombinationQuestionTest extends SpockTest {
 
         then: "both links exist"
         def result = questionRepository.findAll().get(0)
-        result.getItems().size() == 3
+        result.getItemsL().size() == 1
+        result.getItemsR().size() == 2
         result.getLinks().size() == 2
         result.getLinks() == links
     }
@@ -143,7 +150,8 @@ class CreateItemCombinationQuestionTest extends SpockTest {
         items.add(itemDto1)
         items.add(itemDto2)
         def links = new ArrayList<LinkDto>()
-        questionDto.getQuestionDetailsDto().setItems(items)
+        questionDto.getQuestionDetailsDto().setItemsL(items)
+        questionDto.getQuestionDetailsDto().setItemsR(items)
         questionDto.getQuestionDetailsDto().setLinks(links)
 
         when:
@@ -153,4 +161,7 @@ class CreateItemCombinationQuestionTest extends SpockTest {
         def exception = thrown(TutorException)
         exception.getErrorMessage() == ErrorMessage.LINK_BETWEEN_SAME_GROUP_ITEMS
     }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
 }
