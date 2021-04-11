cffd953329e59af60faada233d79bc28339798a4
obernardoquinteiro <bernardoquinteiro@gmail.com>
2021-04-12 00:08:44 +0100

    close #107 create export choice questions tests

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/webservice/ExportMultipleChoiceQuestion.groovy
@@ -0,0 +1,70 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.webservice
+
+import groovyx.net.http.RESTClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.MultipleChoiceQuestionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.OptionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.User
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ExportMultipleChoiceQuestion extends SpockTest {
+    @LocalServerPort
+    private int port
+
+    def courseExecutionDto
+
+    def question
+
+    def setup() {
+        restClient = new RESTClient("http://localhost:" + port)
+        createExternalCourseAndExecution()
+        def questionDto = new QuestionDto()
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.SUBMITTED.name())
+        def optionDto = new OptionDto()
+        optionDto.setContent(OPTION_1_CONTENT)
+        optionDto.setCorrect(true)
+        def options = new ArrayList<OptionDto>()
+        options.add(optionDto)
+        questionDto.setQuestionDetailsDto(new MultipleChoiceQuestionDto())
+        questionDto.getQuestionDetailsDto().setOptions(options)
+        questionService.createQuestion(externalCourse.getId(), questionDto)
+        question = questionRepository.findAll().get(0)
+    }
+
+    def "teacher exports a course"() {
+        given: 'a demon teacher'
+        def teacher
+        teacher = new User(USER_1_NAME, USER_1_EMAIL, USER_1_EMAIL,
+                User.Role.TEACHER, false, AuthUser.Type.TECNICO)
+        teacher.authUser.setPassword(passwordEncoder.encode(USER_1_PASSWORD))
+        teacher.addCourse(externalCourseExecution)
+        externalCourseExecution.addUser(teacher)
+        userRepository.save(teacher)
+        createdUserLogin(USER_1_EMAIL, USER_1_PASSWORD)
+        and: 'prepare request response'
+        restClient.handler.failure = { resp, reader ->
+            [response:resp, reader:reader]
+        }
+        restClient.handler.success = { resp, reader ->
+            [response:resp, reader:reader]
+        }
+
+        when: "the web service is invoked"
+        def map = restClient.get(
+                path: "/courses/" + externalCourseExecution.getId() + "/questions/export",
+                requestContentType: "application/json"
+        )
+
+        then: "the response status is OK"
+        assert map['response'].status == 200
+        assert map['reader'] != null
+    }
+}
