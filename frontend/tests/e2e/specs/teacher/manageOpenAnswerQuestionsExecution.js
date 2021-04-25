8cfbc936d9ead5834ada1ae215dd5dc3d6d4601b
TmsPaiva <tmspaiva@gmail.com>
2021-04-25 19:53:51 +0100

    Close #145

--- frontend/tests/e2e/specs/teacher/manageOpenAnswerQuestionsExecution.js
+++ frontend/tests/e2e/specs/teacher/manageOpenAnswerQuestionsExecution.js
@@ -93,6 +93,32 @@ describe('Manage Open Answer Questions Walk-through', () => {
     );
   });
 
+  it('Can view question (with button)', function () {
+    cy.get('tbody tr')
+      .first()
+      .within(($list) => {
+        cy.get('button').contains('visibility').click();
+      });
+
+    validateQuestion(
+      'Cypress Question Example - 01',
+      'Cypress Question Example - Content - 01'
+    );
+
+    cy.get('button').contains('close').click();
+  });
+
+  it('Can view question (with click)', function () {
+    cy.get('[data-cy="questionTitleGrid"]').first().click();
+
+    validateQuestion(
+      'Cypress Question Example - 01',
+      'Cypress Question Example - Content - 01'
+    );
+
+    cy.get('button').contains('close').click();
+  });
+
   it('Can update title (with right-click)', function () {
     cy.route('PUT', '/questions/*').as('updateQuestion');
 
@@ -123,30 +149,95 @@ describe('Manage Open Answer Questions Walk-through', () => {
     );
   });
 
-  it('Can view question (with button)', function () {
-        cy.get('tbody tr')
-          .first()
-          .within(($list) => {
-            cy.get('button').contains('visibility').click();
-          });
+  it('Can update content (with button)', function () {
+    cy.route('PUT', '/questions/*').as('updateQuestion');
 
-        validateQuestion(
-          'Cypress Question Example - 01',
-          'Cypress Question Example - Content - 01'
-        );
-
-        cy.get('button').contains('close').click();
+    cy.get('tbody tr')
+      .first()
+      .within(($list) => {
+        cy.get('button').contains('edit').click();
       });
 
-      it('Can view question (with click)', function () {
-        cy.get('[data-cy="questionTitleGrid"]').first().click();
+    cy.get('[data-cy="createOrEditQuestionDialog"]')
+      .parent()
+      .should('be.visible')
+      .within(($list) => {
+        cy.get('span.headline').should('contain', 'Edit Question');
 
-        validateQuestion(
-          'Cypress Question Example - 01',
-          'Cypress Question Example - Content - 01'
-        );
+        cy.get('[data-cy="questionQuestionTextArea"]')
+          .clear({ force: true })
+          .type('Cypress New Content For Question!', { force: true });
 
-        cy.get('button').contains('close').click();
+        cy.get('button').contains('Save').click();
       });
 
+    cy.wait('@updateQuestion').its('status').should('eq', 200);
+
+    validateQuestionFull(
+      (title = 'Cypress Question Example - 01 - Edited'),
+      (content = 'Cypress New Content For Question!')
+    );
+  });
+
+  // missing update all with questions as well and change data. Should also be tested for errors :D
+    // missing update all with questions as well and change data. Should also be tested for errors :D
+      // missing update all with questions as well and change data. Should also be tested for errors :D
+
+
+  it('Can duplicate question', function () {
+    cy.get('tbody tr')
+      .first()
+      .within(($list) => {
+        cy.get('button').contains('cached').click();
+      });
+
+    cy.get('[data-cy="createOrEditQuestionDialog"]')
+      .parent()
+      .should('be.visible');
+
+    cy.get('span.headline').should('contain', 'New Question');
+
+    cy.get('[data-cy="questionTypeInput"]')
+      .type('open_answer', { force: true })
+      .click({ force: true });
+
+    cy.wait(1000);
+
+    cy.get('[data-cy="questionTitleTextArea"]')
+      .should('have.value', 'Cypress Question Example - 01 - Edited')
+      .type('{end} - DUP', { force: true });
+    cy.get('[data-cy="questionQuestionTextArea"]').should(
+      'have.value',
+      'Cypress New Content For Question!'
+    );
+    cy.get(
+      '[data-cy="questionAnswerTextArea"]'
+    ).type('Cypress Question Example - Answer - 01', { force: true });
+
+    cy.route('POST', '/courses/*/questions/').as('postQuestion');
+
+    cy.get('button').contains('Save').click();
+
+    cy.wait('@postQuestion').its('status').should('eq', 200);
+
+    cy.get('[data-cy="questionTitleGrid"]')
+      .first()
+      .should('contain', 'Cypress Question Example - 01 - Edited - DUP');
+
+    validateQuestionFull(
+      'Cypress Question Example - 01 - Edited - DUP',
+      'Cypress New Content For Question!'
+    );
+  });
+
+  it('Can delete created question', function () {
+    cy.route('DELETE', '/questions/*').as('deleteQuestion');
+    cy.get('tbody tr')
+      .first()
+      .within(($list) => {
+        cy.get('button').contains('delete').click();
+      });
+
+    cy.wait('@deleteQuestion').its('status').should('eq', 200);
+  });
 });
